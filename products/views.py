4556f9d442f553bc4a1ef5e2c14dcd53cb4210f7
Liam Tate <mrtea@protonmail.com>
2022-02-07 14:02:14 +0000

    searcha dn linked categories

--- products/views.py
+++ products/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q #used for 'or' logic to search in title and description
+from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
-    """ view for all prudcts and sorting and searhing queries """
+    """ view for all products and sorting and searhing queries """
 
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories) #queries in django often use __
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "you didn't search for anything DUMB DUMB")
+                return redirect (reverse('products'))
+
+            queries = Q(name__icontains = query) | Q(description__icontains = query) #the i makes things case insensitive
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
