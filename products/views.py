eb837d9b077c29297f50d3e642f2cb5a12ec3191
Liam Tate <mrtea@protonmail.com>
2022-02-11 15:08:11 +0000

    sort by dropdown working

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q #used for 'or' logic to search in title and description
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -22,6 +23,9 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey == 'category__name' # a double underscore __ allows us to drill into a related model
 
             if 'direction' in request.GET:
                 if 'direction' == 'desc':
