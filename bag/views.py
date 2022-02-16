a19b165f2d196c22e0909bd77325790081facc20
Liam Tate <mrtea@protonmail.com>
2022-02-16 12:06:43 +0000

    shopping bag display

--- bag/views.py
+++ bag/views.py
@@ -3,20 +3,21 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def view_bag(request):
-    """ this exists to return a view of the bag page """
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """ adds quanittiy """
+    """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(request.POST.get('quantity')) #comes from template as a string
-    redirect_url = request.POST.get('redirect')
-    bag = request.session.get('bag', {}) #get bag variable if it exists, or create it if it doesn;t
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
 
-    request.session['bag'] = bag #overwrite the session with new info for bag
-    return(redirect_url)
+    request.session['bag'] = bag
+    return redirect(redirect_url)
