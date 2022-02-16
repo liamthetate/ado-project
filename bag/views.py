600fb3161836b198574c4647f73eafb63354e822
Liam Tate <mrtea@protonmail.com>
2022-02-16 10:46:56 +0000

    bag views and url

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_bag(request):
     """ this exists to return a view of the bag page """
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """ adds quanittiy """
+
+    quantity = int(request.POST.get('quantity')) #comes from template as a string
+    redirect_url = request.POST.get('redirect')
+    bag = request.session.get('bag', {}) #get bag variable if it exists, or create it if it doesn;t
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag #overwrite the session with new info for bag
+    return(redirect_url)
