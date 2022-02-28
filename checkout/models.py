4809fa9d56edc837f922a940d06a552619a653a2
Liam Tate <mrtea@protonmail.com>
2022-02-28 13:23:33 +0000

    stripe stuff kinda broken though lol

--- checkout/models.py
+++ checkout/models.py
@@ -20,13 +20,17 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+
 
     def _generate_order_number(self): #the _ at the start means its priviate to this class
         """ generate a unique ID """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        self.order_total = self.lineitems.aggegrate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggegrate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -10,6 +11,24 @@ from bag.contexts import bag_contents # returns dictionary
 import stripe
 
 # Create your views here.
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, ('Sorry, your payment cannot be '
+                                 'processed right now. Please try '
+                                 'again later.'))
+        return HttpResponse(content=e, status=400)
+
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -30,7 +49,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order_form.save()
+            order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag_items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -64,7 +87,7 @@ def checkout(request):
             return redirect(reverse('checkout_success',
                                     args=[order.order_number]))
         else:
-        messages.error(request, ('There was an error with your form. '
+            messages.error(request, ('There was an error with your form. '
                                     'Please double check your information.'))
     else:
         bag = request.session.get('bag',{})
@@ -77,10 +100,9 @@ def checkout(request):
         stripe_total = round(total * 100) #makes total an interger for Stripe
         stripe.api_key = stripe_public_key
         intent = stripe.PaymentIntent.create(
-            amount=stripe_total
-            currency=settings.STRIPE_CURRENCY
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
         )
-
         order_form = OrderForm()
 
         if not stripe_public_key:
