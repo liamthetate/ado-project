d8bebf4ee164c3e5e5bf490024126e43e9ffb6b3
Liam Tate <mrtea@protonmail.com>
2022-03-03 12:45:16 +0000

    email

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,9 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+
 from bag.contexts import bag_contents # returns dictionary
 
 import stripe
@@ -100,30 +103,74 @@ def checkout(request):
         stripe_total = round(total * 100) #makes total an interger for Stripe
         stripe.api_key = stripe_public_key
         intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-        )
-        order_form = OrderForm()
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+            )
 
-        if not stripe_public_key:
-            messages.warning(request, 'stripe public key is missing')
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except  UserProfile.DoesNotExist:
+                order_form = OrderForm()
+            
+        else:
+            order_form = OrderForm()
 
-        template = 'checkout/checkout.html'
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': intent.client_secret,
-        }
+    if not stripe_public_key:
+        messages.warning(request, 'stripe public key is missing')
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
 
     return render(request, template, context)
 
 
 def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order successfull \
-        your order number is {order_number}. \
-            A confirmation email bla bla {order.email}')
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
@@ -132,4 +179,5 @@ def checkout_success(request, order_number):
     context = {
         'order': order,
     }
+
     return render(request, template, context)
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
+from django.core.mail import send_mail #for emails
+from django.template.loader import render_to_string #for emails
+from django.conf import settings #for emails
+
 from .models import Order, OrderLineItem
 from products.models import Product
 
@@ -10,6 +14,22 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL })
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            cust_email,
+        )
     
     def handle_event(self, event):
         #unexpected event
